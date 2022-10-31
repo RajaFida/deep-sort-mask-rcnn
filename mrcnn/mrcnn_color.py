296de215206bb72dda2822c13fceeccd7ae9a82b
Raja Fida <49719932+RajaFida@users.noreply.github.com>
2022-10-31 09:46:45 +0530

    Update mrcnn_color.py

--- mrcnn/mrcnn_color.py
+++ mrcnn/mrcnn_color.py
@@ -8,17 +8,18 @@ import colorsys
 import os
 
 # 识别80类
-COCO_CLASSES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-               'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
-               'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-               'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag',
-               'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite',
-               'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-               'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
-               'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
-               'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+# COCO_CLASSES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+#                'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
+#                'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+#                'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag',
+#                'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite',
+#                'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+#                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+#                'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+#                'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+#                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
+#                'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+COCO_CLASSES = ['BG', 'Pothole']
 
 # 预测配置
 class InferenceConfig(Config):
@@ -30,7 +31,9 @@ class InferenceConfig(Config):
     IMAGES_PER_GPU = 1
 
     # 物体的分类个数，COCO中共有80种物体+背景
-    NUM_CLASSES = 1 + 80  # background + 80 shapes
+    #NUM_CLASSES = 1 + 80  # background + 80 shapes
+    NUM_CLASSES = 1 + 1
+    
 
 class MRCNN(object):
     def __init__(self, model_path, image_size, min_score):
